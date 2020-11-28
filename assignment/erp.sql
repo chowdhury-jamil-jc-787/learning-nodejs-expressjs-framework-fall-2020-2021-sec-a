2c1c656f3b407f910e0c9715591ce7930cafeba8
jc <17-34747-2@student.aiub.edu>
2020-11-28 17:51:02 +0600

    database upload

---
+++ assignment/erp.sql
@@ -0,0 +1,298 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 28, 2020 at 12:49 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `erp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
+  `id` int(250) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `author` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`id`, `name`, `author`) VALUES
+(1, 'harry potter', 'j.k');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `author` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL,
+  `cemail` varchar(250) NOT NULL,
+  `cpass` varchar(250) NOT NULL,
+  `cname` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`id`, `cemail`, `cpass`, `cname`) VALUES
+(1, 'jk@gmail.com', '1234', 'jk'),
+(2, 'p@gmail.com', '1234', 'jk');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `deliveryman`
+--
+
+CREATE TABLE `deliveryman` (
+  `id` int(11) NOT NULL,
+  `demail` varchar(250) NOT NULL,
+  `dpass` varchar(250) NOT NULL,
+  `dname` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `id` int(250) NOT NULL,
+  `eemail` varchar(250) NOT NULL,
+  `epass` varchar(250) NOT NULL,
+  `ename` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`id`, `eemail`, `epass`, `ename`) VALUES
+(1, 'iftu@gmail.com', '1234', 'iftu'),
+(2, 'opi1@gmail.com', '1234', 'opi1'),
+(3, 'opi2@gmail.com', '1234', 'opi2'),
+(4, 'opi12@gmail.com', '1234', 'opi12'),
+(5, 'opi4@gmail.com', '1234', 'opi4'),
+(6, 'opi5@gmail.com', '1234', 'opi5'),
+(7, 'nafi4@gmail.com', '1234', 'nafi4'),
+(8, 'jk1@gmail.com', '1234', 'jk1'),
+(9, 'gh@gmail.com', '1234', 'gh');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manager`
+--
+
+CREATE TABLE `manager` (
+  `id` int(250) NOT NULL,
+  `memail` varchar(250) NOT NULL,
+  `mpass` varchar(250) NOT NULL,
+  `mname` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `manager`
+--
+
+INSERT INTO `manager` (`id`, `memail`, `mpass`, `mname`) VALUES
+(1, 'jc@gmail.com', '1234', 'jc'),
+(2, 'opi@gmail.com', '1234', 'opi'),
+(3, 'nafi@gmail.com', '1234', 'nafi'),
+(4, 'nafi1', '1234', 'nafi1'),
+(5, 'nafi12', '1234', 'nafi12'),
+(6, 'jc', '1234', 'jc'),
+(7, 'jc1', '1234', 'jc1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `phone_no` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `phone_no`) VALUES
+(15, 'opi2@gmail.com', 'opi2@gmail.com', '0131'),
+(17, 'js@gg', 'js@gg', '013'),
+(18, 'jk1', 'jk1@gmail.com', '01399'),
+(20, 'opi@gmail.com', 'opi@gmail.com', '0130');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_image`
+--
+
+CREATE TABLE `users_image` (
+  `id` int(5) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `mob_no` int(11) NOT NULL,
+  `user_name` varchar(20) NOT NULL,
+  `password` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users_image`
+--
+
+INSERT INTO `users_image` (`id`, `first_name`, `last_name`, `image`, `mob_no`, `user_name`, `password`) VALUES
+(1, 'jc', 'chowdhury', 'dsPmVU.png', 1304962073, 'jc', '1234');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `deliveryman`
+--
+ALTER TABLE `deliveryman`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `manager`
+--
+ALTER TABLE `manager`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users_image`
+--
+ALTER TABLE `users_image`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `book`
+--
+ALTER TABLE `book`
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `deliveryman`
+--
+ALTER TABLE `deliveryman`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `manager`
+--
+ALTER TABLE `manager`
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `users_image`
+--
+ALTER TABLE `users_image`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
